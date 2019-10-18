2bbfb294afb3cc82d84fa5d2cff1899ef29735e3
Justin Nguyen <justinhnguyen88@gmail.com>
2019-10-18 09:38:26 -0700

    Updates

---
+++ precache-manifest.b49abd4a635f1595e4812e7b97d9e0fc.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "140d73295b21abb4a84f05b7db24dc6d",
+    "url": "/reelmovies/index.html"
+  },
+  {
+    "revision": "0c73d1ec8a8fec50461e",
+    "url": "/reelmovies/static/js/2.2124a665.chunk.js"
+  },
+  {
+    "revision": "41f039a842039f1e7d0b",
+    "url": "/reelmovies/static/js/main.c2cd0fa9.chunk.js"
+  },
+  {
+    "revision": "10e8d4dfa751ef763250",
+    "url": "/reelmovies/static/js/runtime-main.af73094d.js"
+  }
+]);
